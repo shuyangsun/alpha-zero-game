c46c81b251975daae986c61eb90c522d109cf1b7
Shuyang Sun <shuyangsun10@gmail.com>
2026-04-13 19:04:51 -0400

    Add build and run instructions to post-generation hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,3 +2,21 @@ import shutil
 
 if "{{ cookiecutter.github_actions }}" != "y":
     shutil.rmtree(".github")
+
+print()
+print("Project generated successfully!")
+print()
+print("Next steps:")
+print()
+print("> cd ./alpha-zero-game-{{ cookiecutter.game_name_kebab }}")
+print()
+print(
+    "> cmake -S cmake_external_project -B cmake_external_project/build -DCMAKE_BUILD_TYPE=Release"
+)
+print()
+print("> cmake --build cmake_external_project/build")
+print()
+print(
+    "> ./cmake_external_project/build/{{ cookiecutter.__game_name_camel }}/{{ cookiecutter.game_slug }} --help"
+)
+print()
