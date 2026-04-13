cfe96f9db299b4202a5e69c9e883a3231c344aea
Shuyang Sun <shuyangsun10@gmail.com>
2026-04-13 18:01:23 -0400

    Refactor project template structure and enhance cookiecutter configuration

---
+++ alpha-zero-game-{{cookiecutter.game_name_kebab}}/include/{{cookiecutter.game_slug}}/game.h
@@ -0,0 +1,25 @@
+#ifndef ALPHA_ZERO_GAME_{{cookiecutter.__include_guard_prj}}_INCLUDE_{{cookiecutter.__include_guard_slug}}_GAME_H_
+#define ALPHA_ZERO_GAME_{{cookiecutter.__include_guard_prj}}_INCLUDE_{{cookiecutter.__include_guard_slug}}_GAME_H_
+
+#include <array>
+#include <cstdint>
+
+#include "alpha-zero-api/game.h"
+
+namespace alphazero::game::{{cookiecutter.game_slug}} {
+
+using {{cookiecutter.__board}} = std::array<std::array<int8_t, 3>, 3>;  // TODO: change type
+using {{cookiecutter.__action}} = int;                                  // TODO: change type
+using {{cookiecutter.__player}} = bool;                                 // TODO: change type
+using {{cookiecutter.__game_interface}} = ::alphazero::api::IGame<{{cookiecutter.__board}}, {{cookiecutter.__action}}, {{cookiecutter.__player}}>;
+
+class {{cookiecutter.__game_cls}}: public {{cookiecutter.__game_interface}} {
+public:
+  {{cookiecutter.__game_cls}}() = default;
+  ~{{cookiecutter.__game_cls}}() override = default;
+};
+
+} // namespace alphazero::game::{{cookiecutter.game_slug}}
+
+#endif  // ALPHA_ZERO_GAME_{{cookiecutter.__include_guard_prj}}_INCLUDE_{{cookiecutter.__include_guard_slug}}_GAME_H_
+
