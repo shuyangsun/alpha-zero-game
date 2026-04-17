80b2a220b20c1f472ebd105441c625e48d472845
Shuyang Sun <shuyangsun10@gmail.com>
2026-04-17 15:42:29 -0400

    Relocate enable_testing and rename constructor test

--- alpha-zero-game-{{cookiecutter.game_name_kebab}}/tests/unit/game_constructors.cc
+++ alpha-zero-game-{{cookiecutter.game_name_kebab}}/tests/unit/game_constructors.cc
@@ -9,7 +9,7 @@ namespace {
 using ::alphazero::game::{{cookiecutter.game_slug}}::{{cookiecutter.__game_interface}};
 using ::alphazero::game::{{cookiecutter.game_slug}}::{{cookiecutter.__player}};
 
-TEST(GameConstructors, MakeUniqueAndCastToInterface) {
+TEST(GameConstructors, DefaultConstructor) {
   std::unique_ptr<const {{cookiecutter.__game_interface}}> game = std::make_unique<{{cookiecutter.__game_cls}}>({{ cookiecutter.__player }}{});
   ASSERT_NE(game, nullptr);
 }
